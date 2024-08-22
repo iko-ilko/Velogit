21a79bac1ff652e58175872823dce3b3b4bc0c4c
ilko <114412252+iko-ilko@users.noreply.github.com>
2024-08-22 20:35:06 +0900

    feat: install necessary packages in velogit.sh

---
+++ srcs/js/post.js
@@ -0,0 +1,7 @@
+//main post
+  //await velog_post
+  //await github_post
+
+//async funtion velog_post()
+//async funtion github_post()
+
--- velogit.sh
+++ velogit.sh
@@ -12,6 +12,7 @@ show_help() {
     echo "예시:"
     echo "  ./velogit.sh post.md     # 'post.md' 파일을 찾아 포스팅"
     echo "  ./velogit.sh --sync      # GitHub과 Velog 동기화"
+    echo "  ./velogit.sh --install   # npm 패키지 설치"
 }
 
 # 파일 찾기 함수
@@ -26,12 +27,23 @@ if [ "$1" == "-h" ] || [ "$1" == "--help" ] || [ -z "$1" ]; then
     show_help
 elif [ "$1" == "-s" ] || [ "$1" == "--sync" ]; then
     node "$SCRIPT_DIR/srcs/js/main.js" sync
+elif [ "$1" == "-i" ] || [ "$1" == "--install" ]; then
+    if [ -f "$SCRIPT_DIR/srcs/packages/package.json" ]; then
+        echo "Installing packages..."
+        cd "$SCRIPT_DIR/srcs/packages"
+        npm install
+        echo "Packages installed successfully."
+        cd "$SCRIPT_DIR"
+    else
+        echo "Error: Can't find package.json file."
+        exit 1
+    fi
 else
     file_path=$(find_file "$1")
     if [ -n "$file_path" ]; then
         node "$SCRIPT_DIR/srcs/js/main.js" post "$file_path"
     else
-        echo "오류: 파일을 찾을 수 없습니다: $1"
+        echo "Error: File not found: $1"
         exit 1
     fi
 fi
